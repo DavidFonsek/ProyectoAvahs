a1e8fccac5f4e1faca55df042ea6894295966db5
jpretelt6 <preteltjuanoficial@gmail.com>
2022-08-13 19:02:27 -0500

    avances en la página principal y novedades

--- avahs/tienda/views.py
+++ avahs/tienda/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, "index.html")
+
+def novedades(request):
+    return render(request, "novedades.html")
