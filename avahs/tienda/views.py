a256daef3bff730cd5a989c5a7c4369ee59715da
samuelgarciacorrea <66627447+samuelgarciacorrea@users.noreply.github.com>
2022-09-05 14:30:26 -0500

    nuevas vistas de la aplicacion

--- avahs/tienda/views.py
+++ avahs/tienda/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from .models import Producto
 
-# Create your views here.
+
+def home(request):
+    productos = Producto.objects.all()
+    data = {
+        'productos': productos
+    } 
+    
+    return render(request, "pages/home.html",data)
+
+def novedades(request):
+    return render(request, "pages/novedades.html")
+
+def descuentos(request):
+    return render(request, "pages/descuentos.html")
+
+def tendencias(request):
+    return render(request, "pages/tendencias.html")
+
+def elemento(request):
+    return render(request, "pages/elemento.html")
+
+def resultados(request):
+    return render(request, "pages/resultados.html")
+
+def carrito(request):
+    return render(request, "pages/carrito.html")
+
+def login(request):
+    return render(request, "registration/login.html")
+ 
+def registro(request):
+    return render(request, "registration/registro.html")
+
+def agregar(request):
+    return render(request,"pages/agregar.html")
+
